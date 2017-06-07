e3a23505a9a2876d22b3b199fff6ed1966ab644e
tomoyayoshikawa <tomoyayoshikawa@yoshikawatomoyanoMacBook-Air.local>
2017-06-07 21:00:53 +0900

    Define actions

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,15 @@
 class GroupsController < ApplicationController
 
   def new
-
+    @group = Group.new
   end
 
   def create
+    if @group = Group.create(create_params)
+      redirect_to :root
+    else
+      render "new"
+  end
 
   end
 
@@ -15,5 +20,9 @@ class GroupsController < ApplicationController
   def update
 
   end
-  
+
+  private
+    def create_params
+      params.require(:group).permit(:name, {:user_ids =>[]})
+    end
 end
