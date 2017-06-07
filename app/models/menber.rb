395a05a5643a99ee24dc91b3a30ad24eedc09973
tomoyayoshikawa <tomoyayoshikawa@yoshikawatomoyanoMacBook-Air.local>
2017-06-07 15:37:11 +0900

    Set associations

--- app/models/menber.rb
+++ app/models/menber.rb
@@ -1,2 +1,4 @@
 class Menber < ApplicationRecord
+  belongs_to :user
+  belongs_to :group
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :menbers
+  has_many :groups, through: :menbers
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
