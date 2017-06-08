bc926f781fb8e09ffeaf967b7a285a72bf0100a3
tomoyayoshikawa <tomoyayoshikawa@yoshikawatomoyanoMacBook-Air.local>
2017-06-09 08:24:38 +0900

    Edit action and method

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,11 +1,16 @@
 class GroupsController < ApplicationController
 
+  before_action :find_group, only: [:edit, :update]
+
+  def index
+  end
+
   def new
     @group = Group.new
   end
 
   def create
-    @group = Group.new(create_params)
+    @group = Group.new(group_params)
     if @group.save
          redirect_to :root, notice:  "グループを作成しました"
       else
@@ -15,15 +20,22 @@ class GroupsController < ApplicationController
 
 
   def edit
-
   end
 
   def update
-
+    if @group.update(group_params)
+      redirect_to :root, notice:  "チャットグループが更新されました。"
+    else
+      render :edit
+    end
   end
 
   private
-    def create_params
+    def group_params
       params.require(:group).permit(:name, {user_ids:[]})
     end
+
+  def find_group
+    @group = Group.find(params[:id])
+  end
 end
