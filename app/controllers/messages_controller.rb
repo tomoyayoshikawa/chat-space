89393a23afe33ff92c08823b41839b6d74e48a16
tomoyayoshikawa <tomoyayoshikawa@yoshikawatomoyanoMacBook-Air.local>
2017-06-09 18:54:52 +0900

    Edit actions in message_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,26 @@
 class MessagesController < ApplicationController
 
-  def index
+  before_action :find_group
 
+  def index
   end
 
+  def create
+    @group.messages.new(message_params)
+    if @group.save
+      redirect_to :group_messages
+    else
+      flash.now[:alert] = "メッセージを入力してください"
+      render :index
+    end
+  end
+
+  def message_params
+    params.require(:message).permit(:body, :image).merge(user_id: current_user.id, group_id: params[:group_id])
+  end
+
+  def find_group
+    @group = Group.find params[:group_id]
+    @messages = Message.new
+  end
 end
