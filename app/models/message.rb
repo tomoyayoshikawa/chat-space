7470b20dac4a75d0b20ba29f5336ce9be2b78ad9
tomoyayoshikawa <tomoyayoshikawa@yoshikawatomoyanoMacBook-Air.local>
2017-06-13 11:45:41 +0900

    Set validation in messages.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,8 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
   validates :body, presence: {if: 'image.blank?'}
+  validates :image, presence: {if: 'body.blank?'}
+  validates :user_id, presence: true
+  validates :group_id, presence: true
   mount_uploader :image, ImageUploader
 end
