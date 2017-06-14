895f5a63fc8377e6bfa7f2da085534fe4a85bbfd
tomoyayoshikawa <tomoyayoshikawa@yoshikawatomoyanoMacBook-Air.local>
2017-06-14 17:45:36 +0900

    Re edit factrygirl to replace password

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,8 +3,8 @@ FactoryGirl.define do
   factory :user do
     name                    { Faker::Name.name }
     email                   { Faker::Internet.email }
-    password                {Faker::Number(8)}
-    password_confirmation   {Faker::Number(8)}
+    password                { Faker::Internet.password }
+    password_confirmation   { password }
   end
 
 end
