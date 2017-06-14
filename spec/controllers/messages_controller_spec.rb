88dcbc860c3bd48f4274e027373f03e8e3e0970d
tomoyayoshikawa <tomoyayoshikawa@yoshikawatomoyanoMacBook-Air.local>
2017-06-14 16:48:25 +0900

    Re edit to replace let()

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -5,6 +5,9 @@ describe MessagesController, type: :controller do
   let(:user) {create(:user)}
   let(:group) {create(:group)}
   let(:message) {create(:message)}
+  let(:get_index) {get :index, params: { group_id: group.id }}
+  let(:post_params) {{ group_id: group.id, user_id: user.id, message: attributes_for(:message) }}
+  let(:unpost_params) {{ group_id: group.id, user_id: user.id, message: attributes_for(:message, body:nil, image: nil) }}
 
   describe 'GET #index' do
     context 'when user_signed_in' do
@@ -13,25 +16,25 @@ describe MessagesController, type: :controller do
       end
 
       it "assigns the requested group to @group" do
-        get :index, params: { group_id: group.id }
+        get_index
         expect(assigns(:group)).to eq group
       end
 
       it "render is the :index template" do
-        get :index, params:{ group_id: group.id }
+        get_index
         expect(response).to render_template :index
       end
 
       it "assigns a new Message to @messages" do
         messages = Message.new
-        get :index, params: { group_id: group.id }
+        get_index
         expect(assigns(:messages)).to be_a_new(Message)
       end
     end
 
     context 'when not user_signed_in' do
       it "redirects to new_user_registration_path" do
-        get :index, params: { group_id: group.id }
+        get_index
         expect(response).to redirect_to new_user_session_path
       end
     end
@@ -45,13 +48,14 @@ describe MessagesController, type: :controller do
 
       it 'saves the new messages in the database' do
           expect{
-          post :create, params: { group_id: group.id, user_id: user.id, message: attributes_for(:message) }
-          }.to change(Message, :count).by(1)
+           post :create, params: post_params
+           }.to change(Message, :count).by(1)
+          # post :create, post_params params: { group_id: group.id, user_id: user.id, message: attributes_for(:message) }
+          # }.to change(Message, :count).by(1)
       end
 
       it 'redirects to messages#index' do
-         post :create,
-         params: { group_id: group.id, user_id: user.id, message: attributes_for(:message) }
+         post :create, params: post_params
          expect(response).to redirect_to group_messages_path(group.id)
       end
     end
@@ -63,12 +67,12 @@ describe MessagesController, type: :controller do
 
       it 'does not save the new message in the database' do
           expect{
-          post :create, params: { group_id: group.id, user_id: user.id, message: attributes_for(:message, body:nil, image: nil) }
+          post :create, params: unpost_params
           }.not_to change(Message, :count)
       end
 
       it "generate error flash message" do
-          post :create, params: { group_id: group.id, user_id: user.id, message: attributes_for(:message, body:nil, image:nil) }
+          post :create, params: unpost_params
           expect(flash[:alert]).to include("メッセージを入力してください")
       end
 
@@ -76,8 +80,7 @@ describe MessagesController, type: :controller do
 
     context 'when not user_signed_in' do
       it 'redirects to new_user_session_path' do
-        post :create,
-        params: { group_id: group.id, user_id: user.id, message: attributes_for(:message) }
+        post :create, params: post_params
         expect(response).to redirect_to new_user_session_path
       end
     end
