fa001f5b60655ad28f6f2330eec782edf4bd3cb4
tomoyayoshikawa <tomoyayoshikawa@yoshikawatomoyanoMacBook-Air.local>
2017-06-18 00:06:51 +0900

    Create function to add users

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -5,6 +5,13 @@ $(function(){
                 '<a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=" '+ user.id +'"data-user-name="'+ user.name +'">追加</a>'+'</div>';
      return html;
     };
+    function findUserHTML(user_id, user_name){
+    var input_html = '<div class="chat-group-user clearfix js-chat-member" id="chat-group-user-8">' +
+                      '<input name="group[user_ids][]" type="hidden" value="'+ user_id +'">' +
+                      '<p class="chat-group-user__name">' + user_name + '</p>'+
+                      '<a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn">削除</a>' + '</div>';
+      return input_html;
+    };
 
     $(document).on('keyup', '#user_search', function(e){
       e.preventDefault();
@@ -30,6 +37,12 @@ $(function(){
      });
 
     $(document).on("click", ".chat-group-user__btn--add",   function(){
-    });
+      $(this).parent().remove();
+      var user_id = $(this).attr("data-user-id");
+      var user_name = $(this).attr("data-user-name");
+      var input_html = findUserHTML(user_id, user_name);
+      $("#user-search-result").append(input_html);
+      });
 });
+
 
