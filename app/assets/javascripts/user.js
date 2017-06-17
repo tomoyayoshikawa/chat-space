5d060010e45e94ec2595721ff3eadef9a48768d0
tomoyayoshikawa <tomoyayoshikawa@yoshikawatomoyanoMacBook-Air.local>
2017-06-18 00:04:05 +0900

    Create event for add--btn

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -4,7 +4,7 @@ $(function(){
                 '<p class="chat-group-user__name">' + user.name + '</p>' +
                 '<a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=" '+ user.id +'"data-user-name="'+ user.name +'">追加</a>'+'</div>';
      return html;
-  };
+    };
 
     $(document).on('keyup', '#user_search', function(e){
       e.preventDefault();
@@ -20,7 +20,6 @@ $(function(){
       .done(function(data){
         var $form = $('.user-search-result')
         $form.find('li').remove();
-
         $(data).each(function(i, user){
           $form.append('<li>' + buildHTML(user) + '</li>')
         });
@@ -28,6 +27,9 @@ $(function(){
       .fail(function() {
         alert('エラーが発生しました');
       });
+     });
+
+    $(document).on("click", ".chat-group-user__btn--add",   function(){
     });
-  });
+});
 
