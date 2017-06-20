af0f3939f986f6363102714a5cee00bcec460b77
tomoyayoshikawa <tomoyayoshikawa@yoshikawatomoyanoMacBook-Air.local>
2017-06-20 11:30:45 +0900

    Modify and create auto-update-function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,19 +1,22 @@
 $(function() {
-  function buidHTML(data) {
-    var name = '<li class= "messages__chat__user__name">' + data.name + '</li>';
+  var $chatview = $('.messages__chat');
 
-    var time = '<li class= "messages__chat__user__time">' + data.time + '</li>';
+  function buildHTML(message) {
+    var name = '<li class= "messages__chat__message__name">' + message.name + '</li>';
 
-    if(data.body){
-      var text = '<p class="messages__chat__message__text">' + data.body + '</p>';
+    var time = '<li class= "messages__chat__message__time">' + message.time + '</li>';
+
+    if(message.body){
+      var text = '<li class="messages__chat__message__text">' + message.body + '</li>';
     };
-    if(data.image) {
-    var imageTag = '<img src ="' + data.image +'">';
-    var image = '<p class ="message_chat__message__image">' + imageTag + '</p>';
-  };
+    if(message.image) {
+    var imageTag = '<img src ="' + message.image +'">';
+    var image = '<li class ="messages_chat__message__image">' + imageTag + '</li>';
+    };
 
-    $('.messages__chat').append(name, time, text, image)
-  };
+    var chat = $('<ul class= "messages__chat__message">').append(name).append(time).append(text).append(image);
+     return chat
+  }
 
   $('.message_form').on('submit', function(e) {
     e.preventDefault();
@@ -29,14 +32,37 @@ $(function() {
        dataType: "json"
      })
      .done(function(data) {
-      buidHTML(data);
+      var chat = buildHTML(data);
+      $chatview.append(chat);
       $('.message_form')[0].reset();
 
-      $('.messages__chat').animate({ scrollTop: $('.messages__chat')[0].scrollHeight }, 'slow');
+      $chatview.animate({ scrollTop: $chatview[0].scrollHeight }, 'slow');
     })
      .fail(function() {
       alert('メッセージを送信できません');
     });
-    return false;
-    });
   });
+
+  $(function(){
+    setInterval(function(){
+      $.ajax({
+        type: 'GET',
+        url: location.href.json,
+        dataType: 'json'
+      })
+      .done(function(data) {
+        var insertHTML = '';
+        var id = $('.messages__chat__message').filter(':last').data('message-id');
+        data.messages.forEach(function(message){
+          if (message.id > id){
+            insertHTML = buildHTML(message);
+          }
+        });
+        $chatview.append(insertHTML);
+      })
+      .fail(function(data){
+        alert('自動更新に失敗しました')
+      });
+    }, 5000 );
+  });
+});
